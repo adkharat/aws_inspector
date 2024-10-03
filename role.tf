b61671f33d5c6ce6361d43b4bd7cdc30e8062022
ajay-kharat <ajay,kharat@ibm.com>
2024-10-03 18:27:45 +0530

    target account

--- role.tf
+++ role.tf
@@ -61,7 +61,6 @@ module "iam_policy" {
   iam_policy_name = "MyAWSServiceRoleForImageBuilderPolicy"
   iam_policy_description = "MyAWSServiceRoleForImageBuilderPolicy"
   iam_policy_json_file_path = file("./AWSServiceRoleForImageBuilderPolicy.json")
-
 }
 
 module "aws_service_role_for_image_builder_policyxx" {
@@ -71,3 +70,35 @@ module "aws_service_role_for_image_builder_policyxx" {
     iam_role_name = module.aws_service_role_for_image_builder_role.name
     iam_policy_arn = module.iam_policy.arn
 }
+
+/*Golden Image Distribution Role for Target Account*/
+
+module "image_distribution_in_target_account_role" {
+  source = "./module/aws_iam_role"
+  iam_role_name = "EC2ImageBuilderDistributionCrossAccountRole"
+  iam_role_trust_policy = file("./allow_image_sharing_trust_policy.json") //Replace SOURCE ACCOUNT in Trust policy
+}
+
+module "cross_account_distribution_policy_attachment" {
+    depends_on = [ module.image_distribution_in_target_account_role ]
+
+    source = "./module/aws_iam_role_policy_attachment"
+    iam_role_name = module.image_distribution_in_target_account_role.name
+    iam_policy_arn = "arn:aws:iam::aws:policy/Ec2ImageBuilderCrossAccountDistributionAccess" //AWS managed policy
+}
+
+module "cross_account_kms_iam_policy" {
+  source = "./module/aws_iam_policy"
+  iam_policy_name = "cross_account_kms_iam_policy"
+  iam_policy_description = "cross_account_kms_iam_policy"
+  iam_policy_json_file_path = file("./cross_account_kms_iam_policy.json")
+}
+
+module "cross_account_kms_iam_policy_attachment" {
+    depends_on = [ module.image_distribution_in_target_account_role ]
+
+    source = "./module/aws_iam_role_policy_attachment"
+    iam_role_name = module.image_distribution_in_target_account_role.name
+    iam_policy_arn = module.cross_account_kms_iam_policy.arn
+}
+
