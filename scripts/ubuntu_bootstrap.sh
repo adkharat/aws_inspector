34e2d2d2dbcd2b53fc679d538190b2549fc9162f
ajay-kharat <ajay,kharat@ibm.com>
2024-10-07 17:09:53 +0530

    aa

--- scripts/ubuntu_bootstrap.sh
+++ scripts/ubuntu_bootstrap.sh
@@ -16,11 +16,11 @@ mv falcon-* falcon-sensor.deb
 chmod +x falcon-sensor.deb
 dpkg -i falcon-sensor.deb
 /opt/CrowdStrike/falconctl -s --cid=2E90DC4F9AF2406CA2363D68D4FB8F83-81 -f --provisioning-token=B26B63CF
-sudo systemcti start falcon-sensor 
+sudo systemctl start falcon-sensor 
 sudo systemctl status falcon-sensor
 sudo aws s3 cp s3://golden-ami-files/Downloads/Linux/agent_installer.sh . --endpoint-url https://s3.us-west-2.amazonaws.com/
 chmod +x agent_installer.sh
 sudo ./agent_installer.gh install_start --token us: f93dle2d-151c-499e-9809-384efb9abc91
-sudo wget https://33.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
 sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
 apt-get update
