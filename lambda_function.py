532631ea924a5b76d99ec661ba70557a3910805d
ajay-kharat <ajay,kharat@ibm.com>
2024-10-12 16:39:05 +0530

    aa

--- lambda_function.py
+++ lambda_function.py
@@ -1,41 +1,74 @@
-import boto3
+# -*- coding: utf-8 -*-
+import os
 import json
+import boto3
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
+ses = boto3.client("ses")
+s3 = boto3.client("s3")
+
+SENDER_EMAIL = "shaikamanulla881@gmail.com"
+TO_EMAILS = ["shaikamanulla881@gmail.com"]
+
+def send_email(sender, receiver, subject, body, file_content, file_name):
+    msg = MIMEMultipart()
+    msg["Subject"] = subject
+    msg["From"] = sender
+    msg["To"] = ",".join(receiver)
+
+    body_txt = MIMEText(body, "html")
+    attachment = MIMEApplication(file_content)
+    attachment.add_header("Content-Disposition", "attachment", filename=file_name)
+
+    msg.attach(body_txt)
+    msg.attach(attachment)
+
+    response = ses.send_raw_email(
+        Source=sender, Destinations=receiver, RaawMessage={"Data": msg.as_string()}
+    )
+
+    return response
 
 def lambda_handler(event, context):
-    
-    # Print the event message for debugging purposes
-    print("Received event: " + json.dumps(event, indent=2))
+    if "Records" in event:
+        records = event["Records"][0]
+        action = records["eventName"]
+        ip = records["requestParameters"]["sourceIPAddress"]
+        bucket_name = records["s3"]["bucket"]["name"]
+        file_object = records["s3"]["object"]["key"]
 
-    for e in event["Records"]:
-        bucketName = e["s3"]["bucket"]["name"]
-        objectName = e["s3"]["object"]["key"]
-        eventName = e["eventName"]
-        
-    print("bucketName --- : " + bucketName)
-    print("objectName --- : " + objectName)
-    print("eventName --- : " + eventName)
+        fileObj = s3.get_object(Bucket=bucket_name, Key=file_object)
+        file_content = fileObj["Body"].read()
+        file_name = file_object.split("/")[-1]
+
+        # Debugging print statements
+        print("bucketName --- : " + bucket_name)
+        print("objectName --- : " + file_object)
+        print("fileObj --- : " + str(fileObj))  # Fixed here
+        print("file_content --- : " + file_content.decode('utf-8'))  # Convert to string
+        print("file_name --- : " + file_name)
     
-    bClient = boto3.client("ses")
-    
-    eSubject = 'Golden Image ' + Vulnerability report generated
+        subject = f"Golden Image Vulnerability report generated on S3:{bucket_name} - {str(action)}"
+        body = """
+            Hey,<br>
         
-    eBody = """
-        <br>
-        Hey,<br>
-        
-        Welcome to AWS S3 notification lambda trigger<br>
-        
-        We are here to notify you that vulnerability report is generated
-        S3 Bucket name : {} <br>
-        Vulnerability report name : {}
-        <br>
-    """.format(bucketName, objectName)
-    
-    send = {"Subject": {"Data": eSubject}, "Body": {"Html": {"Data": eBody}}}
-    print("Before sending --- : ")
-    result = bClient.send_email(Source= "shaikamanulla881@gmail.com", Destination= {"ToAddresses": ["shaikamanulla881@gmail.com"]}, Message= send)
-    print("After sending --- : ")
-    return {
-        'statusCode': 200,
-        'body': json.dumps(result)
-    }    
+            Welcome to AWS S3 notification<br>
+            
+            We are here to notify you that vulnerability report is generated.<br>
+            This email is to notify you regarding {} event.<br>
+            Vulnerability report {} is Uploaded.<br>
+            Source IP: {}<br>
+        """.format(
+            action, file_object, ip  # Corrected 'object' to 'file_object'
+        )
+
+        response = send_email(
+            SENDER_EMAIL, TO_EMAILS, subject, body, file_content, file_name
+        )
+
+        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+            return {"statusCode": 200, "body": json.dumps("Email sent successfully!")}
+
+    return {"statusCode": 500, "body": json.dumps("Email delivery failed!")}
