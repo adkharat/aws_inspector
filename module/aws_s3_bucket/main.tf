bca47b3c8415e0f8307974aa101e17142d8773fa
ajay-kharat <ajay,kharat@ibm.com>
2024-11-02 00:27:51 +0530

    fg

--- module/aws_s3_bucket/main.tf
+++ module/aws_s3_bucket/main.tf
@@ -2,4 +2,12 @@
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = var.s3_bucket_name
     # bucket_prefix = var.s3_bucket_prefix 
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "aws:kms"
+          kms_master_key_id = "aws/s3"
+        }
+      }
+    }
 }
